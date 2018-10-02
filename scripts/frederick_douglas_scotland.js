3c5d81aba46ed75c36fc0a4d887aba004bcdf83e
Chris Fleet <c.fleet@nls.uk>
2018-10-02 16:18:42 +0100

    Add files via upload

---
+++ scripts/frederick_douglas_scotland.js
@@ -0,0 +1,34 @@
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "1", "No": "1", "Address": "Arbroath", "City": "10th February 1846", "ID": 1 }, "geometry": { "type": "Point", "coordinates": [ -2.5817083865, 56.5584386671, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "2", "No": "2", "Address": "Arbroath", "City": "11-12th February 1846", "ID": 2 }, "geometry": { "type": "Point", "coordinates": [ -2.5816264, 56.561575, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "3", "No": "3", "Address": "Ayr", "City": "23-24th March  1846", "ID": 3 }, "geometry": { "type": "Point", "coordinates": [ -4.6341246537, 55.4641417827, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "4", "No": "4", "Address": "Ayr", "City": "27th March 1860", "ID": 4 }, "geometry": { "type": "Point", "coordinates": [ -4.6350150959, 55.4640493989, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "5", "No": "5", "Address": "Dundee", "City": "27-28th January 1846 and 10th March 1846", "ID": 5 }, "geometry": { "type": "Point", "coordinates": [ -2.9743184745, 56.4601266966, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "6", "No": "6", "Address": "Dundee", "City": "30th January 1846; 28th September 1846", "ID": 6 }, "geometry": { "type": "Point", "coordinates": [ -2.975165855, 56.4624937386, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "7", "No": "7", "Address": "Dundee", "City": "9th February 1846", "ID": 7 }, "geometry": { "type": "Point", "coordinates": [ -2.9762384076, 56.4576533859, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "8", "No": "8", "Address": "Dundee", "City": "13th February 1860", "ID": 8 }, "geometry": { "type": "Point", "coordinates": [ -2.9716418842, 56.4610949773, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "9", "No": "9", "Address": "Falkirk", "City": "6th February 1860", "ID": 9 }, "geometry": { "type": "Point", "coordinates": [ -3.7893742, 56.0002832, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "10", "No": "10", "Address": "Fenwick", "City": null, "ID": 10 }, "geometry": { "type": "Point", "coordinates": [ -4.445858905, 55.6540237025, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "11", "No": "11", "Address": "Glasgow", "City": "15th January 1846; 21-23rd April 1846; 30th September 1846; 1st October 1846;", "ID": 11 }, "geometry": { "type": "Point", "coordinates": [ -4.2448228853, 55.8587867796, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "12", "No": "12", "Address": "Glasgow", "City": "19-21st February 1846", "ID": 12 }, "geometry": { "type": "Point", "coordinates": [ -4.2524049, 55.8645196, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "13", "No": "13", "Address": "Glasgow", "City": "2nd October 1846", "ID": 13 }, "geometry": { "type": "Point", "coordinates": [ -4.262612977, 55.8569643307, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "14", "No": "14", "Address": "Glasgow", "City": "14th February 1860", "ID": 14 }, "geometry": { "type": "Point", "coordinates": [ -4.2475148847, 55.8602595528, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "15", "No": "15", "Address": "Glasgow", "City": "26th March 1860", "ID": 15 }, "geometry": { "type": "Point", "coordinates": [ -4.2792508649, 55.8666188354, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "16", "No": "16", "Address": "Glasgow", "City": "28th March 1860", "ID": 16 }, "geometry": { "type": "Point", "coordinates": [ -4.2371843121, 55.8563060182, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "17", "No": "17", "Address": "Greenock", "City": "10 April 1846", "ID": 17 }, "geometry": { "type": "Point", "coordinates": [ -4.7613646157, 55.949956685, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "18", "No": "18", "Address": "Greenock", "City": "c.20thJanuary -c.18th February 1860", "ID": 18 }, "geometry": { "type": "Point", "coordinates": [ -4.756053945, 55.9479121549, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19", "No": "19", "Address": "Hawick", "City": "1-14 April 1846", "ID": 19 }, "geometry": { "type": "Point", "coordinates": [ -2.7936163735, 55.4209144188, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "20", "No": "20", "Address": "Kirkcaldy", "City": "1st June 1846; 22nd October 1846", "ID": 20 }, "geometry": { "type": "Point", "coordinates": [ -3.1618349171, 56.1064770514, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "21", "No": "21", "Address": "Montrose", "City": "9th March 1846", "ID": 21 }, "geometry": { "type": "Point", "coordinates": [ -2.4647757, 56.7132486, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "22", "No": "22", "Address": "Paisley", "City": "17-20th March 1846; 30th March 1846; 6th April 1846", "ID": 22 }, "geometry": { "type": "Point", "coordinates": [ -4.4213207572, 55.8448989122, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "23", "No": "23", "Address": "Paisley", "City": "17th April 1846", "ID": 23 }, "geometry": { "type": "Point", "coordinates": [ -4.4243271, 55.8458892, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "24", "No": "24", "Address": "Paisley", "City": "23rd September 1846", "ID": 24 }, "geometry": { "type": "Point", "coordinates": [ -4.4256883515, 55.8427392954, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "25", "No": "25", "Address": "Paisley", "City": "6 april 1846 and 23rd september 1846", "ID": 25 }, "geometry": { "type": "Point", "coordinates": [ -4.425689473, 55.8427364661, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "26", "No": "26", "Address": "Paisley", "City": "25th April 1846", "ID": 26 }, "geometry": { "type": "Point", "coordinates": [ -4.4363704934, 55.8414628156, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "27", "No": "27", "Address": "Perth", "City": "23\/26th January 1846; 24th October 1846", "ID": 27 }, "geometry": { "type": "Point", "coordinates": [ -3.4295724363, 56.3959160438, 0.0 ] } }
+]
+}
