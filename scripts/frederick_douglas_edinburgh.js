bbf3042aede3463bd0e561fc979d385b9adb9e8b
Chris Fleet <c.fleet@nls.uk>
2018-10-02 16:15:30 +0100

    Add files via upload

---
+++ scripts/frederick_douglas_edinburgh.js
@@ -0,0 +1,17 @@
+{
+"type": "FeatureCollection",
+"name": "frederick_douglas_edinburgh_5_Aug",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Unique ID": "FDE1", "Address": "Music Hall, Assembly Rooms, 54 George Street", "ID": 7 }, "geometry": { "type": "Point", "coordinates": [ -3.1986754, 55.9528298 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE2", "Address": "York Temperance Hotel, 19 Nicolson Street", "ID": 3 }, "geometry": { "type": "Point", "coordinates": [ -3.1861882, 55.9469748 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE3", "Address": "Rose Street Chapel, 19 Rose Street", "ID": 6 }, "geometry": { "type": "Point", "coordinates": [ -3.1959644777, 55.953073420199999 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE4", "Address": "Brighton Street Church, Brighton Street", "ID": 1 }, "geometry": { "type": "Point", "coordinates": [ -3.1895839327, 55.946668795199997 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE5", "Address": "33 Gilmore Place", "ID": 10 }, "geometry": { "type": "Point", "coordinates": [ -3.2055946217, 55.941512233600001 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE6", "Address": "Waterloo Rooms, 29 Waterloo Place", "ID": 5 }, "geometry": { "type": "Point", "coordinates": [ -3.1856988, 55.954102 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE7", "Address": "Regent Road", "ID": 4 }, "geometry": { "type": "Point", "coordinates": [ -3.188642689884587, 55.952490991769587 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE8", "Address": "45 Melville Street", "ID": 9 }, "geometry": { "type": "Point", "coordinates": [ -3.2146931954, 55.9495483189 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE9", "Address": "Queen Street Hall, 5 Queen Street", "ID": 8 }, "geometry": { "type": "Point", "coordinates": [ -3.1957767567, 55.955109612400001 ] } },
+{ "type": "Feature", "properties": { "Unique ID": "FDE10", "Address": "South College Street Church, 11a South College Street", "ID": 2 }, "geometry": { "type": "Point", "coordinates": [ -3.1870256603, 55.946888162699999 ] } }
+]
+}
